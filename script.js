85a4b4053d5d0c9d7fe5e0953db7f434dfbc2356
Peter Leite <peterleite.dev@gmail.com>
2021-09-25 18:16:36 -0300

    arquivos do projeto

---
+++ script.js
@@ -0,0 +1,13 @@
+onload = ()=>{
+    let nome = localStorage.getItem('nome')
+    let h1 = document.getElementById('nome')
+    h1.innerHTML = nome
+}
+
+function update(element){
+    let value = element.value;
+    let h1 = document.getElementById('nome')
+    h1.innerHTML = value;
+
+    localStorage.setItem('nome', value)
+}
